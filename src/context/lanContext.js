113bd71bef180deaddb6b2fa903ddaf749e808cc
Luis_Gomez_Ojeda <92588124+luisGomezOjeda@users.noreply.github.com>
2023-10-19 16:33:30 -0300

    Delete src directory

--- src/context/lanContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const LanContext = createContext()
-
-export default LanContext
--- src/helpers/helpSeparator.js
+++
@@ -1,8 +0,0 @@
-export function helpSeparator(number) {
-    const parts = number.toString().split('.');
-    
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-
-    return parts.join('.');
-}
-

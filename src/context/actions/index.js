8f54780ba43bd5332297294cd4098b312c588908
Luis_Gomez_Ojeda <92588124+luisGomezOjeda@users.noreply.github.com>
2024-03-24 13:01:18 -0300

    Delete src directory

--- src/context/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const serchEs = (name) => {
-    return ({type : "SERCH_ES", payload : {name}})
-}
-
-export const serchEn = (url) => {
-    return ({type : "SEARCH_EN", payload : {url}})
-}
--- src/pages/PageHome/StyledHome.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components"
-
-export const StyledHome = styled.section`
-    height: auto;
-    min-height: 60vh;
-`;
